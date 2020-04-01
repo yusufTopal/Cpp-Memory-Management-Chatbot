41797fd6f96e4c824cd9260e2e88434b168d78e1
yusufTopal <yusuftpl00@gmail.com>
2020-04-01 22:05:29 +0200

    Task-0  and Task-1

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -34,11 +34,13 @@ ChatBot::~ChatBot()
 {
     std::cout << "ChatBot Destructor" << std::endl;
 
+    
     // deallocate heap memory
-    if(_image != NULL) // Attention: wxWidgets used NULL and not nullptr
+    if(_image != NULL ) // Attention: wxWidgets used NULL and not nullptr
     {
         delete _image;
-        _image = NULL;
+        _image= NULL;
+     
     }
 }
 
